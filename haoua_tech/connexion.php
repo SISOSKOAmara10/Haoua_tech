def597f72f97f9b257f52d076ce11b4d5c3e6285
SISSOKOAmara10 <101343904+SISOSKOAmara10@users.noreply.github.com>
2022-05-24 03:55:38 +0000

    Update connexion.php

--- haoua_tech/connexion.php
+++ haoua_tech/connexion.php
@@ -1,13 +1,30 @@
-<?php  
-	session_start();
-try {
-	$strConnection= 'mysql:host=localhost;dbname=haoua_tech';
-	$pdo = new PDO ($strConnection,'root','');
-
-	} 
-catch (PDOException $e) {
-	
-$msg = 'ERREUR PDO dans'  . $e->getMessage();
-die ($msg);
-}
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        
+        <?php
+            $servname = 'localhost';
+            $dbname = 'haoua_tech';
+            $user = 'root';
+            $pass = '';
+            
+            try{
+                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "INSERT INTO serveur(proprietaire,pays,Adresse,numero,Mail)
+                        VALUES('ESCAE','Mali','Hamdallaye',',8300780,','eascae@escae.ml')";
+                
+                $dbco->exec($sql);
+                echo 'Donnée ajoutée dans la table';
+            }
+            
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+        ?>
+    </body>
+</html>
