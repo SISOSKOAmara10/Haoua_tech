0355d5079a7e2fd0474c6c669745ee8c59b7710f
SISSOKOAmara10 <101343904+SISOSKOAmara10@users.noreply.github.com>
2022-05-24 03:53:00 +0000

    Update Ajouter_serveur.php

--- haoua_tech/Ajouter_serveur.php
+++ haoua_tech/Ajouter_serveur.php
@@ -1,40 +1,30 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Ajouter Serveur</title>
-<link rel="stylesheet" href="asset/CSS/Ajouterserveur.css">
-</head>
-<body>
+    <head>
+        <title>Creation de la Table</title>
+    </head>
+    <body>
+        <h1>Creation de la base</h1>  
+        <?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            
+            try{
+                $dbco = new PDO("mysql:host=$servername", $username, $password);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "CREATE DATABASE haoua_tech";
+                $dbco->exec($sql);
+                
+                echo 'La base est créée.';
+            }
+            
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+        ?>
 
-    <fieldset>
-    <form method="post" action="traitementserveur.php">
-             <h1>Ajouter Serveur</h1>
-
-           <button type="button" class="close" aria-label="Close" id="close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-
-            <label for="Proprietaire" id="propritaire">Propriétaire</label>
-            <input type="text" name="proprietaire"><br><br>
-
-            <label for="Pays" id="Pays">Pays</label>
-            <input type="text" name="Pays"><br><br>
-
-            <label for="Localite" id="localite">Localité</label>
-            <input type="text" name="Localite"><br><br>
-
-            <label for="Contact" id="">Contact</label>
-            <input type="tel" name="Contact"><br><br>
-
-            <label for="adresse" id="">Site</label>
-            <input type="text" name="adresse"><br><br>
-
-            <div id="button">
-                    <imput type ="reset" value="reset"><button>Réinitialiser</button></imput>
-                    <imput type="submit" value="submit"><button>Ajouter</button></imput>
-            </div>
-
-    </form>
-    </fieldset>
-</body>
+        
+    </body>
 </html>
