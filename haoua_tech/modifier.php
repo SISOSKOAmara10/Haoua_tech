192c6f9d8012200d274483b35dd552c20a2cfa50
SISSOKOAmara10 <101343904+SISOSKOAmara10@users.noreply.github.com>
2022-05-24 03:57:47 +0000

    Update modifier.php

--- haoua_tech/modifier.php
+++ haoua_tech/modifier.php
@@ -1,57 +1,52 @@
-<?php
-include 'functions.php';
-$pdo = pdo_connect_mysql();
-$msg = '';
-// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
-if (isset($_GET['id'])) {
-    if (!empty($_POST)) {
-        // This part is similar to the create.php, but instead we update a record and not insert
-        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
-        $name = isset($_POST['name']) ? $_POST['name'] : '';
-        $email = isset($_POST['email']) ? $_POST['email'] : '';
-        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-        $title = isset($_POST['title']) ? $_POST['title'] : '';
-        $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
-        // Update the record
-        $stmt = $pdo->prepare('UPDATE contacts SET id = ?, name = ?, email = ?, phone = ?, title = ?, created = ? WHERE id = ?');
-        $stmt->execute([$id, $name, $email, $phone, $title, $created, $_GET['id']]);
-        $msg = 'Updated Successfully!';
-    }
-    // Get the contact from the contacts table
-    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
-    $stmt->execute([$_GET['id']]);
-    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
-    if (!$contact) {
-        exit('Contact doesn\'t exist with that ID!');
-    }
-} else {
-    exit('No ID specified!');
-}
-?>
-
-
-<?=template_header('Read')?>
-
-<div class="content update">
-	<h2>Update Contact #<?=$contact['id']?></h2>
-    <form action="update.php?id=<?=$contact['id']?>" method="post">
-        <label for="id">ID</label>
-        <label for="name">Name</label>
-        <input type="text" name="id" placeholder="1" value="<?=$contact['id']?>" id="id">
-        <input type="text" name="name" placeholder="John Doe" value="<?=$contact['name']?>" id="name">
-        <label for="email">Email</label>
-        <label for="phone">Phone</label>
-        <input type="text" name="email" placeholder="johndoe@example.com" value="<?=$contact['email']?>" id="email">
-        <input type="text" name="phone" placeholder="2025550143" value="<?=$contact['phone']?>" id="phone">
-        <label for="title">Title</label>
-        <label for="created">Created</label>
-        <input type="text" name="title" placeholder="Employee" value="<?=$contact['title']?>" id="title">
-        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i', strtotime($contact['created']))?>" id="created">
-        <input type="submit" value="Update">
-    </form>
-    <?php if ($msg): ?>
-    <p><?=$msg?></p>
-    <?php endif; ?>
-</div>
-
-<?=template_footer()?>
+<!DOCTYPE html>
+<html>
+    <head>
+                    
+    </head>
+    <body>
+         
+        <?php
+            $servname = "localhost"; $dbname = "haoua_tech"; $user = "root"; $pass = "";
+            
+            try{
+                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                //Crée la table Users
+                $sql = "CREATE TABLE Users (
+                  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                  Prenom VARCHAR(30) NOT NULL,
+                  Nom VARCHAR(30) NOT NULL,
+                  Mail VARCHAR(50) NOT NULL
+                )";
+                $dbco->exec($sql);
+                
+                //On prépare la requête et on lie les paramètres
+                $sth = $dbco->prepare("
+                  INSERT INTO Users (Prenom, Nom, Mail)
+                  VALUES (:prenom, :nom, :mail)
+                ");
+                $sth->bindParam(':prenom', $prenom);
+                $sth->bindParam(':nom', $nom);
+                $sh->bindParam(':mail', $mail);
+                
+                //Insère une première entrée
+                $prenom = "Aliou"; $nom = "Coulibaly"; $mail = "aliuoucoulibaly@yahoo.com";
+                $sth->execute();
+                
+                //Insère une deuxième entrée
+                $prenom = "Sadio"; $nom = "Camara"; $mail = "camara1sadd@yahoo.com";
+                $sth->execute();
+                
+                //Insère une troisième entrée
+                $prenom = "Kanoute"; $nom = "Aliou"; $mail = "kanoute.a@gmail.com";
+                $sth->execute();
+                echo "Bravo";
+                }                
+                           
+            catch(PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+            }
+        ?>
+    </body>
+</html>
